bbed17c0aa63022da4f8545277c9e4b5d5d58ecf
neil-ca <ulisescamps9@gmail.com>
2023-09-28 22:18:36 -0600

    consuming and converting the video

--- src/converter/convert/mp3converter.py
+++ src/converter/convert/mp3converter.py
@@ -1,8 +1,9 @@
-import pika 
+import pika
 import json
 import tempfile
 import os
 from bson.objectid import ObjectId
+import moviepy.editor
 
 
 def start(message, fs_videos, fs_mp3s, channel):
@@ -11,4 +12,30 @@ def start(message, fs_videos, fs_mp3s, channel):
     out = fs_videos.get(ObjectId["video_fid"])
 
     tf.write(out.read())
+    # Create audio from temp video file
+    audio = moviepy.editor.VideoFileClip(tf.name).audio
+    tf.close()
 
+    tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
+    audio.write_audiofile(tf_path)
+
+    # Save file to mongo
+    f = open(tf_path, "rb")
+    data = f.read()
+    fid = fs_mp3s.put(data)
+    f.close()
+    os.remove(tf_path)
+
+    message["mp3_fid"] = str(fid)
+    try:
+        channel.basic_publish(
+            exchnge="",
+            routing_key=os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                    delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            )
+        )
+    except Exception as err:
+        fs_mp3s.delete(fid)
+        return "failed to publish message"
